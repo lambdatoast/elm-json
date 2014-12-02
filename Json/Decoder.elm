d8f8eed91d6d66d20fa1afa96b7ed08f13d1e20f
Alexander Noriega <lambdatoast@gmail.com>
2014-12-02 08:24:28 -0300

    Add more decodeN functions up to 8. Closes #3.

--- Json/Decoder.elm
+++ Json/Decoder.elm
@@ -105,4 +105,50 @@ decode3 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) g json =
                   >>= fa3 >>= 
                   process (g a1 a2)))
 
+decode4 : NamedDec a1 -> NamedDec a2 -> NamedDec a3 -> NamedDec a4 -> (a1 -> a2 -> a3 -> a4 -> b) -> Decoder b
+decode4 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) (NDec x4 fa4) g json = 
+  getVal x1 json >>= fa1 >>=  (\a1 -> 
+  getVal x2 json >>= fa2 >>=  (\a2 -> 
+  getVal x3 json >>= fa3 >>=  (\a3 -> 
+  getVal x4 json >>= fa4 >>=  (\a4 -> output <| g a1 a2 a3 a4) ) ) )
+
+decode5 : NamedDec a1 -> NamedDec a2 -> NamedDec a3 -> NamedDec a4 -> NamedDec a5 -> (a1 -> a2 -> a3 -> a4 -> a5 -> b) -> Decoder b
+decode5 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) (NDec x4 fa4) (NDec x5 fa5) g json = 
+  getVal x1 json >>= fa1 >>=  (\a1 -> 
+  getVal x2 json >>= fa2 >>=  (\a2 -> 
+  getVal x3 json >>= fa3 >>=  (\a3 -> 
+  getVal x4 json >>= fa4 >>=  (\a4 -> 
+  getVal x5 json >>= fa5 >>=  (\a5 -> output <| g a1 a2 a3 a4 a5) ) ) ) )
+
+decode6 : NamedDec a1 -> NamedDec a2 -> NamedDec a3 -> NamedDec a4 -> NamedDec a5 -> NamedDec a6 -> (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> b) -> Decoder b
+decode6 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) (NDec x4 fa4) (NDec x5 fa5) (NDec x6 fa6) g json = 
+  getVal x1 json >>= fa1 >>=  (\a1 -> 
+  getVal x2 json >>= fa2 >>=  (\a2 -> 
+  getVal x3 json >>= fa3 >>=  (\a3 -> 
+  getVal x4 json >>= fa4 >>=  (\a4 -> 
+  getVal x5 json >>= fa5 >>=  (\a5 -> 
+  getVal x6 json >>= fa6 >>=  (\a6 -> output <| g a1 a2 a3 a4 a5 a6) ) ) ) ) )
+
+decode7 : NamedDec a1 -> NamedDec a2 -> NamedDec a3 -> NamedDec a4 -> NamedDec a5 -> NamedDec a6 -> NamedDec a7 -> (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> a7 -> b) -> Decoder b
+decode7 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) (NDec x4 fa4) (NDec x5 fa5) (NDec x6 fa6) (NDec x7 fa7) g json = 
+  getVal x1 json >>= fa1 >>=  (\a1 -> 
+  getVal x2 json >>= fa2 >>=  (\a2 -> 
+  getVal x3 json >>= fa3 >>=  (\a3 -> 
+  getVal x4 json >>= fa4 >>=  (\a4 -> 
+  getVal x5 json >>= fa5 >>=  (\a5 -> 
+  getVal x6 json >>= fa6 >>=  (\a6 -> 
+  getVal x7 json >>= fa7 >>=  (\a7 -> output <| g a1 a2 a3 a4 a5 a6 a7) ) ) ) ) ) )
+
+decode8 : NamedDec a1 -> NamedDec a2 -> NamedDec a3 -> NamedDec a4 -> NamedDec a5 -> NamedDec a6 -> NamedDec a7 -> NamedDec a8 -> (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> a7 -> a8 -> b) -> Decoder b
+decode8 (NDec x1 fa1) (NDec x2 fa2) (NDec x3 fa3) (NDec x4 fa4) (NDec x5 fa5) (NDec x6 fa6) (NDec x7 fa7) (NDec x8 fa8) g json = 
+  getVal x1 json >>= fa1 >>=  (\a1 -> 
+  getVal x2 json >>= fa2 >>=  (\a2 -> 
+  getVal x3 json >>= fa3 >>=  (\a3 -> 
+  getVal x4 json >>= fa4 >>=  (\a4 -> 
+  getVal x5 json >>= fa5 >>=  (\a5 -> 
+  getVal x6 json >>= fa6 >>=  (\a6 -> 
+  getVal x7 json >>= fa7 >>=  (\a7 -> 
+  getVal x8 json >>= fa8 >>=  (\a8 -> output <| g a1 a2 a3 a4 a5 a6 a7 a8) ) ) ) ) ) ) )
+
+
 decode = decode1
